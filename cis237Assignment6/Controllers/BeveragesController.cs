58f84bd1168bd7d55f9b6d076772c3a033b3fb7c
Andy Cullen <thisisandycullen@gmail.com>
2015-12-10 16:45:44 -0500

    Added filter fields

--- cis237Assignment6/Controllers/BeveragesController.cs
+++ cis237Assignment6/Controllers/BeveragesController.cs
@@ -18,7 +18,72 @@ namespace cis237Assignment6.Controllers
         // GET: Beverages
         public ActionResult Index()
         {
-            return View(db.Beverages.ToList());
+            //----------------------------------------------------ADDED BY ME-------------------------------------------------------
+
+            //SETUP A VARIABLE TO HOLD THE BEVERAGES DATA SET
+            DbSet<Beverage> BeveragesToSearch = db.Beverages;
+
+            //SETUP SOME STRING TO HOLD THE DATA THAT MIGHT BE IN THE SESSION.
+            //IF THERE IS NOTHING IN THE SESSION WE CAN STILL USE THESE VARIABLES
+            //AS A DEFAULT VALUE.
+            string filterName = "";
+            string filterPack = "";
+            string filterMinPrice = "";
+            string filterMaxPrice = "";
+            string filterActive = "";
+
+            decimal minPrice = 0m;
+            decimal maxPrice = 100m;
+
+            if (Session["name"] != null && String.IsNullOrWhiteSpace((string)Session["name"]))
+            {
+                filterName = (string)Session["name"];
+            }
+
+            if (Session["pack"] != null && String.IsNullOrWhiteSpace((string)Session["pack"]))
+            {
+                filterPack = (string)Session["pack"];
+            }
+
+            if (Session["minPrice"] != null && String.IsNullOrWhiteSpace((string)Session["minPrice"]))
+            {
+                filterMinPrice = (string)Session["minPrice"];
+                minPrice = Decimal.Parse(filterMinPrice);
+
+            }
+
+            if (Session["maxPrice"] != null && String.IsNullOrWhiteSpace((string)Session["maxPrice"]))
+            {
+                filterMaxPrice = (string)Session["maxPrice"];
+                maxPrice = Decimal.Parse(filterMaxPrice);
+            }
+
+            if (Session["active"] != null && String.IsNullOrWhiteSpace((string)Session["active"]))
+            {
+                filterActive = (string)Session["active"];
+            }
+
+            //FILTER THE BEVERAGESTOSEARCH DATASET. USE THE WHERE THAT WE USED BEFORE WHEN DOING EF WORK,
+            //ONLY THIS TIME SEND IN MORE LAMBDA EXPRESSIONS TO NARROW IT DOWN FURTHER.
+            IEnumerable<Beverage> filtered = BeveragesToSearch.Where(beverage => beverage.price >= minPrice &&
+                                                                                 beverage.price <= maxPrice &&
+                                                                                 beverage.name.Contains(filterName));
+
+            //CONVERT THE DATABASE SET TO A LIST NOW THAT THE QUERY WORK IS DONE ON IT.
+            IEnumerable<Beverage> finalFiltered = filtered.ToList();
+
+            //PLACE THE STRING REPRESENTATION OF THE VALUES IN THE SESSION INTO THE VIEWBAG.
+            ViewBag.filterName = filterName;
+            ViewBag.filterPack = filterPack;
+            ViewBag.filterMinPrice = filterMinPrice;
+            ViewBag.filterMaxPrice = filterMaxPrice;
+            ViewBag.filterActive = filterActive;
+
+            //RETURN THE VIEW WITH A FILTERED SELECTION OF BEVERAGES
+            return View(finalFiltered);
+
+            //-------------------------------------------------------ADDED BY ME END  ---------------------------------------------------
+
         }
 
         // GET: Beverages/Details/5
@@ -123,6 +188,32 @@ namespace cis237Assignment6.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+//----------------------------------------------------------------ADDED BY ME------------------------------------------------------------------
+        [HttpPost, ActionName("Filter")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Filter()
+        {
+            //GET THE FORM DATA THAT AS SENT OUT OF THE REQUEST OBJECT.
+            //THE STRING THAT IS USED AS A KEY TO GET THE DATA MATCHES THE
+            //PROPERTY OF THE FORM CONTROL
+            String name = Request.Form.Get("name");
+            String pack = Request.Form.Get("pack");
+            String minPrice = Request.Form.Get("minPrice");
+            String maxPrice = Request.Form.Get("maxPrice");
+            String active = Request.Form.Get("active");
+
+            //STORE THE FORM DATA INTO THE SESSION SO THAT IT CAN BE RETRIEVED 
+            //LATER TO FILTER THE DATA
+            Session["name"] = name;
+            Session["pack"] = pack;
+            Session["minPrice"] = minPrice;
+            Session["maxPrice"] = maxPrice;
+            Session["active"] = active;
+
+            //REDIRECT TO THE INDEX
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ cis237Assignment6/Scripts/beverages.js
@@ -0,0 +1,9 @@
+﻿//You almost always need this first document ready function. Code goes inside it.
+$(document).ready(function () {
+    //This is a jQuery selector. The '.' is to select an element with the class that
+    //follows the '.', the '#' is to select an element with the id that follows the '#'.
+    $(".dataRow").click(function (firedEvent) {
+        console.log("The event fired!");
+        //document.location = "/Beverages/Edit/[id_goes_here!!!]";
+    });
+});
